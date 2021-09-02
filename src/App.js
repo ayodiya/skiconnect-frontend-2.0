d200dbafb3b08f66247a34bef7f2b507b196c06c
Ayodeji Oludiya <devayodiya@gmail.com>
2021-09-02 10:43:21 +0100

    frontend: add suspense,routes and footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { Suspense } from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { theme } from './utils/theme'
 import { ThemeProvider } from '@material-ui/core/styles'
 
+import Routes from './routes'
+import Loading from './components/Loading'
 import Navbar from './components/navbar'
+import Footer from './components/Footer'
 
 function App () {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Navbar />
+      <Router>
+        <Suspense fallback={<Loading />}>
+          <Navbar />
+          <Routes />
+          <Footer />
+        </Suspense>
+      </Router>
     </ThemeProvider>
   )
 }
